02ca0f11a10a805e99ba16575545fe1291b3b41c
Brien Colwell <xcolwell@gmail.com>
2024-12-02 15:44:13 -0800

    build changes to support ios preview

--- network/URnetworkSdk/Package.swift
+++ network/URnetworkSdk/Package.swift
@@ -1,17 +1,21 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+// see https://developer.apple.com/documentation/xcode/distributing-binary-frameworks-as-swift-packages
 let package = Package(
 	name: "URnetworkSdk",
+	platforms: [
+        .macOS(.v11), .iOS(.v14)
+    ],
 	products: [
 		.library(
 			name: "URnetworkSdk",
-			targets: ["URnetworkSdk"]
+			targets: ["URnetworkSdkBinary"]
 		),
 	],
 	targets: [
 		.binaryTarget(
-			name: "URnetworkSdk",
+			name: "URnetworkSdkBinary",
 			path: "../../../sdk/build/ios/URnetworkSdk.xcframework"
 		),
 	]
