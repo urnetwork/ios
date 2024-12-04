3dab2ec89ea8f0985f79e2edd8a079d70c55beb1
Brien Colwell <xcolwell@gmail.com>
2024-12-03 16:06:11 -0800

    update swift package

--- network/URnetworkSdk/Package.swift
+++ network/URnetworkSdk/Package.swift
@@ -4,9 +4,6 @@ import PackageDescription
 // see https://developer.apple.com/documentation/xcode/distributing-binary-frameworks-as-swift-packages
 let package = Package(
 	name: "URnetworkSdk",
-	platforms: [
-        .macOS(.v11), .iOS(.v14)
-    ],
 	products: [
 		.library(
 			name: "URnetworkSdk",
@@ -18,5 +15,9 @@ let package = Package(
 			name: "URnetworkSdkBinary",
 			path: "../../../sdk/build/ios/URnetworkSdk.xcframework"
 		),
+
+        .testTarget(
+            name: "URnetworkSdkTests",
+            dependencies: ["URnetworkSdkBinary"]),
 	]
 )
