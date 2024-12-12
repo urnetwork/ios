e53ceea64a79a0173b63b93205b68359223349f1
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2024-12-13 08:15:28 +0900

    :shower: cleanup unused code

--- network/network/Main/Connect/ConnectView.swift
+++ network/network/Main/Connect/ConnectView.swift
@@ -17,8 +17,6 @@ struct ConnectView: View {
     
     var logout: () -> Void
     
-    // @State var bottomSheetPosition: BottomSheetPosition = .relativeBottom(0.2)
-    
     init(api: SdkBringYourApi, logout: @escaping () -> Void) {
         _viewModel = StateObject.init(wrappedValue: ViewModel(
             api: api
@@ -32,87 +30,85 @@ struct ConnectView: View {
     
     var body: some View {
         
-        // ZStack {
+        VStack {
+            Text("Connect View!!")
             
-            VStack {
-                Text("Connect View!!")
-                
-                Spacer().frame(height: 32)
-                
-                Button(action: logout) {
-                    Text("Logout")
-                }
-
+            Spacer().frame(height: 32)
+            
+            Button(action: logout) {
+                Text("Logout")
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .bottomSheet(
-                bottomSheetPosition: $viewModel.bottomSheetPosition,
-                switchablePositions: viewModel.bottomSheetSwitchablePositions,
-                headerContent: {
-                    
-                    VStack {
-                     
-                        if let selectedProvider = viewModel.selectedProvider {
-                            ProviderListItemView(
-                                name: selectedProvider.name,
-                                providerCount: selectedProvider.providerCount,
-                                color: viewModel.getProviderColor(selectedProvider),
-                                isSelected: false,
-                                setSelectedProvider: {}
-                            )
-                        } else {
-                            ProviderListItemView(
-                                name: "Best available provider",
-                                providerCount: nil,
-                                color: Color.urCoral,
-                                isSelected: false,
-                                setSelectedProvider: {
-                                    viewModel.setSelectedProvider(nil)
-                                }
-                            )
-                        }
-                        
-                        HStack {
-                            Image(systemName: "magnifyingglass")
-                                .foregroundColor(themeManager.currentTheme.textFaintColor)
-                            TextField("", text: $viewModel.searchQuery, prompt: Text("Search")
-                                      // placeholder color
-                                .foregroundColor(themeManager.currentTheme.textFaintColor)
-                            )
-                            // color of entered text
-                            .foregroundColor(themeManager.currentTheme.textMutedColor)
-                        }
-                        .padding(.vertical, 8)
-                        .padding(.horizontal, 5)
-                        .background(RoundedRectangle(cornerRadius: 10).fill(themeManager.currentTheme.tintedBackgroundBase))
-                        .padding(.horizontal, 16)
-                        
-                        Spacer().frame(height: 16)
-                        
+
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .bottomSheet(
+            bottomSheetPosition: $viewModel.bottomSheetPosition,
+            switchablePositions: viewModel.bottomSheetSwitchablePositions,
+            headerContent: {
+                
+                VStack {
+                 
+                    if let selectedProvider = viewModel.selectedProvider {
+                        ProviderListItemView(
+                            name: selectedProvider.name,
+                            providerCount: selectedProvider.providerCount,
+                            color: viewModel.getProviderColor(selectedProvider),
+                            isSelected: false,
+                            setSelectedProvider: {}
+                        )
+                    } else {
+                        ProviderListItemView(
+                            name: "Best available provider",
+                            providerCount: nil,
+                            color: Color.urCoral,
+                            isSelected: false,
+                            setSelectedProvider: {
+                                viewModel.setSelectedProvider(nil)
+                            }
+                        )
                     }
                     
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(themeManager.currentTheme.textFaintColor)
+                        TextField("", text: $viewModel.searchQuery, prompt: Text("Search")
+                                  // placeholder color
+                            .foregroundColor(themeManager.currentTheme.textFaintColor)
+                        )
+                        // color of entered text
+                        .foregroundColor(themeManager.currentTheme.textMutedColor)
+                    }
+                    .padding(.vertical, 8)
+                    .padding(.horizontal, 5)
+                    .background(RoundedRectangle(cornerRadius: 10).fill(themeManager.currentTheme.tintedBackgroundBase))
+                    .padding(.horizontal, 16)
+                    
+                    Spacer().frame(height: 16)
+                    
                 }
-            ) {
-                
-                ProviderListSheetView(
-                    selectedProvider: viewModel.selectedProvider,
-                    setSelectedProvider: viewModel.setSelectedProvider,
-                    providerCountries: viewModel.providerCountries,
-                    providerPromoted: viewModel.providerPromoted,
-                    providerDevices: viewModel.providerDevices,
-                    providerRegions: viewModel.providerRegions,
-                    providerCities: viewModel.providerCities,
-                    providerBestSearchMatches: viewModel.providerBestSearchMatches
-                )
                 
             }
-            .dragIndicatorColor(themeManager.currentTheme.textFaintColor)
-            .customBackground(
-                themeManager.currentTheme.backgroundColor
-                    .cornerRadius(12)
-                    .shadow(color: themeManager.currentTheme.borderBaseColor, radius: 1, x: 0, y: 0)
+        ) {
+            
+            ProviderListSheetView(
+                selectedProvider: viewModel.selectedProvider,
+                setSelectedProvider: viewModel.setSelectedProvider,
+                providerCountries: viewModel.providerCountries,
+                providerPromoted: viewModel.providerPromoted,
+                providerDevices: viewModel.providerDevices,
+                providerRegions: viewModel.providerRegions,
+                providerCities: viewModel.providerCities,
+                providerBestSearchMatches: viewModel.providerBestSearchMatches
             )
-            .enableAppleScrollBehavior()
+            
+        }
+        .dragIndicatorColor(themeManager.currentTheme.textFaintColor)
+        .customBackground(
+            themeManager.currentTheme.backgroundColor
+                .cornerRadius(12)
+                .shadow(color: themeManager.currentTheme.borderBaseColor, radius: 1, x: 0, y: 0)
+        )
+        .enableAppleScrollBehavior()
     }
 }
 
