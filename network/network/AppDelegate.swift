8aebadb11bffcd6418c09e8731d900d0f6090b4c
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2024-12-30 18:04:19 +0900

    lock orientation to portrait on iphone

---
+++ network/network/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  URnetwork
+//
+//  Created by Stuart Kuentzel on 2024/12/30.
+//
+
+import Foundation
+import UIKit
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            return .allButUpsideDown
+        } else {
+            return .portrait
+        }
+    }
+}
