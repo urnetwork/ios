db709540fc6ff05b87b4f3ab3803ed79732dc38b
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2025-01-16 18:12:06 +0900

    delete account

--- network/network/Main/Account/Settings/SettingsViewModel.swift
+++ network/network/Main/Account/Settings/SettingsViewModel.swift
@@ -14,6 +14,13 @@ extension SettingsView {
     @MainActor
     class ViewModel: ObservableObject {
         
+        var api: SdkApi
+        
+        init(api: SdkApi) {
+            self.api = api
+            checkNotificationSettings()
+        }
+        
         @Published var canReceiveNotifications: Bool = false {
             didSet {
                 if canReceiveNotifications == true {
@@ -22,11 +29,13 @@ extension SettingsView {
             }
         }
         
-        let domain = "SettingsViewModel"
+        /**
+         * Delete account
+         */
+        @Published var isPresentedDeleteAccountConfirmation: Bool = false
+        @Published var isDeletingNetwork: Bool = false
         
-        init() {
-            checkNotificationSettings()
-        }
+        let domain = "SettingsViewModel"
         
         private func checkNotificationSettings() {
             let center = UNUserNotificationCenter.current()
@@ -67,6 +76,69 @@ extension SettingsView {
             
         }
         
+        func deleteAccount() async -> Result<Void, Error> {
+            
+            if isDeletingNetwork {
+                return .failure(NetworkDeleteError.inProgress)
+            }
+            
+            DispatchQueue.main.async {
+                self.isDeletingNetwork = true
+            }
+                
+            do {
+                
+                let result: SdkNetworkDeleteResult = try await withCheckedThrowingContinuation { [weak self] continuation in
+                
+                    guard let self = self else { return }
+                    
+                    let callback = NetworkDeleteCallback { result, err in
+                        
+                        if let err = err {
+                            continuation.resume(throwing: err)
+                            return
+                        }
+                        
+                        guard let result = result else {
+                            continuation.resume(throwing: SendPasswordResetLinkError.resultInvalid)
+                            return
+                        }
+                        
+                        continuation.resume(returning: result)
+                        
+                    }
+                    
+                    api.networkDelete(callback)
+                    
+                }
+                   
+                DispatchQueue.main.async {
+                    self.isDeletingNetwork = false
+                }
+                
+                return .success(())
+                
+            }
+            catch(let error) {
+                DispatchQueue.main.async {
+                    self.isDeletingNetwork = false
+                }
+                return .failure(error)
+            }
+            
+        }
+        
     }
     
+}
+
+private class NetworkDeleteCallback: SdkCallback<SdkNetworkDeleteResult, SdkNetworkDeleteCallbackProtocol>, SdkNetworkDeleteCallbackProtocol {
+    func result(_ result: SdkNetworkDeleteResult?, err: Error?) {
+        handleResult(result, err: err)
+    }
+}
+
+enum NetworkDeleteError: Error {
+    case inProgress
+    case resultInvalid
 }
