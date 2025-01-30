5979699d68cdf431c2cdc79f3fd9f786c7ea9921
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2025-01-30 20:54:37 +0900

    fixes tap-to-connect animation bug

--- app/network/Main/Connect/ConnectButton/ConnectCanvasDisconnectedState/ConnectCanvasDisconnectedStateView.swift
+++ app/network/Main/Connect/ConnectButton/ConnectCanvasDisconnectedState/ConnectCanvasDisconnectedStateView.swift
@@ -12,31 +12,37 @@ struct ConnectCanvasDisconnectedStateView: View {
     @EnvironmentObject var themeManager: ThemeManager
     
     @State private var pulseScale: CGFloat = 1
-    @State private var pulseOpacity: Double = 1
+    // @State private var pulseOpacity: Double = 1
+    
+    @State var showAnimation: Bool = false
     
     var body: some View {
         
         ZStack {
             
-            ZStack {
+            if showAnimation {
              
-                Circle()
-                    .fill(Color.urElectricBlue)
-                    .frame(width: 56, height: 56)
-                    .scaleEffect(pulseScale)
-                    .opacity(pulseOpacity)
-                    .animation(
-                        Animation.easeOut(duration: 1.5)
-                            .repeatForever(autoreverses: false),
-                        value: pulseScale
-                    )
+                // pulsing blue circle
+                ZStack(alignment: .center) {
+                 
+                    Circle()
+                        .fill(Color.urElectricBlue)
+                        .frame(width: 56, height: 56)
+                        .scaleEffect(pulseScale)
+                        .opacity(1.5 - pulseScale)
+                        .animation(
+                            Animation.easeOut(duration: 1.5)
+                                .repeatForever(autoreverses: false),
+                            value: pulseScale
+                        )
+                    
+                }
+                .frame(width: 84, height: 84, alignment: .center)
+                .contentShape(Rectangle())
+                .onAppear {
+                    pulseScale = 1.5
+                }
                 
-            }
-            .frame(width: 84, height: 84)
-            .contentShape(Rectangle())
-            .onAppear {
-                pulseScale = 1.5
-                pulseOpacity = 0
             }
             
             
@@ -62,7 +68,18 @@ struct ConnectCanvasDisconnectedStateView: View {
             }
         }
         .frame(width: 120, height: 120)
+        .onAppear {
+            /**
+             * https://stackoverflow.com/a/65610266/3703043
+             * Wait for the view to fully render before showing the animated element.
+             * This prevents unpredictable shift in animated circle position
+             */
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
+                showAnimation = true
+            }
+        }
     }
+
 }
 
 #Preview {
